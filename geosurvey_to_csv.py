aacdb2b3100f8e545559233ba5a5f5c4c7246b57
Hrishikesh Ballal <hrishikeshballal@yahoo.com>
2020-01-30 10:38:13 +0000

    Update geosurvey_to_csv.py

--- geosurvey_to_csv.py
+++ geosurvey_to_csv.py
@@ -1,44 +1,33 @@
 import json
 import config
 import requests
+import csv
 import math
-import shapely
-from shapely.geometry import shape, mapping, shape, asShape
-import fiona, json
-from fiona import collection
-from fiona.crs import from_string
 
 if __name__ == "__main__":
 
-	session = requests.Session()
+    session = requests.Session()
 
-	def get_jobs(page=0):
-		url = config.apisettings['job_url']
-		first_page = session.get(url).json()
-		yield first_page
-		count = math.ceil(first_page['count']/10)
+    def get_jobs(page=0):
+        url = config.apisettings['job_url']
+        first_page = session.get(url).json()
+        yield first_page
+        count = math.ceil(first_page['count']/10)
 
-		for page in range(2, count + 1):
-			next_page = session.get(url, params={'page': page}).json()
-			yield next_page
+        for page in range(2, count + 1):
+            next_page = session.get(url, params={'page': page}).json()
+            yield next_page
 
-	downloaded_data_results = []
+    downloaded_data_results = []
 
-	for page in get_jobs():
-		downloaded_data_results += page['results']
+    for page in get_jobs():
+        downloaded_data_results += page['results']
 
-	print("Downloaded %s responses from survey" % len(downloaded_data_results))
+    print("Downloaded %s responses from survey" % len(downloaded_data_results))
 
-
-	schema = {
-		'geometry': 'Polygon',
-		'properties': {'id':'str', 'comment':'str','category':'str','date_added':'str'}
-	}
-	crs = from_string("+datum=WGS84 +ellps=WGS84 +no_defs +proj=longlat")
-	with collection('downloaded_data.gpkg', 'w', driver='GPKG',crs=crs, layer = 'polygons',schema=schema) as c:		
-		for curPoly in downloaded_data_results:
-			s = asShape(curPoly['geojson']['geometry'])
-			c.write({
-				'geometry': mapping(s),
-				'properties': {'id': curPoly['id'], 'comment':curPoly['comment'], 'category':curPoly['category'], 'date_added':curPoly['date_added']}
-				})
+    with open('survey_results.csv', mode='w') as survey_results:
+        employee_writer = csv.writer(survey_results, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        
+        employee_writer.writerow(['Comment', 'Category', 'Date-Time added'])
+        for current_row in downloaded_data_results:
+            employee_writer.writerow([current_row['comment'], current_row['category'], current_row['date_added']])
